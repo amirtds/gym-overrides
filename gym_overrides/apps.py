5026a5a84f7ba156d889c5beb4e47ffba35e5e6c
Amir Tadrisi <amirtds@gmail.com>
2024-09-04 12:10:14 -0500

    chore: change apps.y to include only common settings

--- gym_overrides/apps.py
+++ gym_overrides/apps.py
@@ -1,15 +1,39 @@
+"""
+Provides application configuration for Gym Overrides.
+As well as default values for running Gym Overrides along with functions to
+add entries to the Django conf settings needed to run Gym Overrides.
+"""
+
 from django.apps import AppConfig
-from edx_django_utils.plugins import PluginSettings, PluginURLs
-from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
+from edx_django_utils.plugins.constants import (
+    PluginURLs, PluginSettings
+)
+from openedx.core.djangoapps.plugins.constants import (
+    ProjectType, SettingsType
+)
+
 
 class GymOverridesConfig(AppConfig):
+    """
+    Provides application configuration for Gym Overrides.
+    """
+
     name = 'gym_overrides'
+    verbose_name = 'gym_overrides'
+
     plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: u'gym_overrides',
+                PluginURLs.REGEX: u'^gym_overrides/',
+                PluginURLs.RELATIVE_PATH: u'urls',
+            }
+        },
         PluginSettings.CONFIG: {
             ProjectType.LMS: {
-                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: 'settings.common'},
-                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: 'settings.production'},
-                SettingsType.TEST: {PluginSettings.RELATIVE_PATH: 'settings.test'},
+                SettingsType.COMMON: {
+                    PluginSettings.RELATIVE_PATH: u'settings.common'
+                },
             }
         }
     }
