4f9eb4385d202ffd0267a02bc6248386373491ec
Amir Tadrisi <amirtds@gmail.com>
2024-09-04 13:09:47 -0500

    fix: generate certificate override function

--- gym_overrides/settings/common.py
+++ gym_overrides/settings/common.py
@@ -2,6 +2,6 @@ def plugin_settings(settings):
     """
     Set of plugin settings used by the Open Edx platform.
     """
-    settings.OVERRIDE_GENERATE_CERTIFICATE = 'gym_overrides.overrides.override_generate_certificate.generate_certificate'
+    settings.OVERRIDE_GENERATE_CERTIFICATE = 'gym_overrides.overrides.override_generate_certificate._generate_certificate'
     settings.OVERRIDE_TRACK_USER_REGISTRATION = 'gym_overrides.overrides.override_track_user_registration.override_track_user_registration'
     settings.OVERRIDE_IS_ELIGIBLE_FOR_CERTIFICATE = 'gym_overrides.overrides.override_is_eligible_for_certificate.is_eligible_for_certificate'
