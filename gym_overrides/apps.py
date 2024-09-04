1788572ff1d33f6c6913dfaa1b32861a21c08df2
Amir Tadrisi <amirtds@gmail.com>
2024-09-03 20:35:30 -0500

    chore: add apps.py

---
+++ gym_overrides/apps.py
@@ -0,0 +1,73 @@
+"""
+App configuration for gym_overrides.
+"""
+
+from django.apps import AppConfig
+try:
+    from openedx.core.djangoapps.plugins.constants import (PluginSettings,
+                                                       PluginURLs, ProjectType,
+                                                       SettingsType)
+    plugin_initiated = True
+except ImportError:
+    plugin_initiated = False
+
+
+EXTENSIONS_APP_NAME = 'gym_overrides'
+
+
+class GymOverridesConfig(AppConfig):
+    """
+    gym-overrides configuration.
+    """
+    name = EXTENSIONS_APP_NAME
+    verbose_name = 'gym-overrides'
+
+    if plugin_initiated:
+        # Class attribute that configures and enables this app as a Plugin App.
+        plugin_app = {
+            PluginURLs.CONFIG: {
+                ProjectType.LMS: {
+                    PluginURLs.NAMESPACE: EXTENSIONS_APP_NAME,
+                    PluginURLs.APP_NAME: EXTENSIONS_APP_NAME,
+                    PluginURLs.REGEX: r'^gym_overrides/',
+                    PluginURLs.RELATIVE_PATH: 'urls',
+                },
+                ProjectType.CMS: {
+                    PluginURLs.NAMESPACE: EXTENSIONS_APP_NAME,
+                    PluginURLs.APP_NAME: EXTENSIONS_APP_NAME,
+                    PluginURLs.REGEX: r'^gym_overrides/',
+                    PluginURLs.RELATIVE_PATH: 'urls',
+                }
+            },
+
+            PluginSettings.CONFIG: {
+                ProjectType.LMS: {
+                    SettingsType.COMMON: {
+                        PluginSettings.RELATIVE_PATH: 'settings.common',
+                    },
+                    SettingsType.TEST: {
+                        PluginSettings.RELATIVE_PATH: 'settings.test',
+                    },
+                    SettingsType.PRODUCTION: {
+                        PluginSettings.RELATIVE_PATH: 'settings.production',
+                    },
+                },
+                ProjectType.CMS: {
+                    SettingsType.COMMON: {
+                        PluginSettings.RELATIVE_PATH: 'settings.common',
+                    },
+                    SettingsType.TEST: {
+                        PluginSettings.RELATIVE_PATH: 'settings.test',
+                    },
+                    SettingsType.PRODUCTION: {
+                        PluginSettings.RELATIVE_PATH: 'settings.production',
+                    },
+                },
+            },
+        }
+
+    def ready(self, *args, **kwargs):
+        """
+        Register signals on app loding.
+        """
+        from . import signals  # noqa: F401
--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup
 setup(
     name='gym_overrides',
     version='0.0.1',
+    author='Amir Tadrisi',
+    author_email='amirtds@gmail.com',
     license='MIT',
     description='Customizing The Gymnasium Open edX Instance Plugin',
+    packages=[
+        'gym_overrides',
+    ],
     entry_points={
         'lms.djangoapp': [
             'gym_overrides = gym_overrides.apps:GymOverridesConfig',
