e524a4241b2f48974bd22b36dece04901716009c
Amir Tadrisi <amirtds@gmail.com>
2024-09-04 23:08:33 -0500

    chore: add cls and prev_fn to is_eligible for cert

--- gym_overrides/overrides/override_is_eligible_for_certificate.py
+++ gym_overrides/overrides/override_is_eligible_for_certificate.py
@@ -1,10 +1,5 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-def is_eligible_for_certificate(mode_slug, status=None):
+def is_eligible_for_certificate(prev_fn, cls, mode_slug, status=None):
     """
     Make all modes eligible for certificates
     """
-    logger.info(f"is_eligible_for_certificate called with mode_slug={mode_slug}, status={status}")
     return True
