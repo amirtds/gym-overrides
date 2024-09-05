60fb513c5abf812b39d010adb68ec26257460289
Amir Tadrisi <amirtds@gmail.com>
2024-09-05 15:41:10 -0500

    chore: remove api key placeholder and add installed apps

--- gym_overrides/settings/common.py
+++ gym_overrides/settings/common.py
@@ -7,7 +7,6 @@ def plugin_settings(settings):
     Set of plugin settings used by the Open Edx platform.
     """
     log.info("Gym Overrides plugin settings loaded")
-    settings.ACCREDIBLE_API_KEY = ''
     settings.OVERRIDE_IS_PASSING_STATUS = 'gym_overrides.overrides.override_is_passing_status.is_passing_status'
     settings.OVERRIDE_GENERATE_CERTIFICATE = 'gym_overrides.overrides.override_generate_certificate._generate_certificate'
     settings.OVERRIDE_TRACK_USER_REGISTRATION = 'gym_overrides.overrides.override_track_user_registration.override_track_user_registration'
