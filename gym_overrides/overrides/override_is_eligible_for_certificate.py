c6ec417b23a18c809c247c604992bbeeed2cf6ed
Amir Tadrisi <amirtds@gmail.com>
2024-09-04 21:29:29 -0500

    chore: remove prev_fn from is_eligible_for_certificate

--- gym_overrides/overrides/override_is_eligible_for_certificate.py
+++ gym_overrides/overrides/override_is_eligible_for_certificate.py
@@ -1,5 +1,10 @@
-def is_eligible_for_certificate(prev_fn, mode_slug, status=None):
+import logging
+
+logger = logging.getLogger(__name__)
+
+def is_eligible_for_certificate(mode_slug, status=None):
     """
     Make all modes eligible for certificates
     """
+    logger.info(f"is_eligible_for_certificate called with mode_slug={mode_slug}, status={status}")
     return True
